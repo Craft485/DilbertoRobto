fe3f2d5b04067b8731eba213c5e4015c3b574255
Craft <38158429+Craft485@users.noreply.github.com>
2020-06-12 22:01:35 -0400

    now takes args for search queries

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Message } = require('discord.js')
 // Used as an extension of String.prototype
-const {} = require('colors')
+require('colors')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const commands = require('./command')
@@ -24,11 +24,16 @@ client.on('message', (msg) => {
         return
     } else if (msg.content.startsWith(config.prefix)) {
         // With only 2 commands I see no reason to have a seperate method for parsing which command is called
-        if (msg.content.substr(1).startsWith('dilbert')) {
-            // Get a random search term
-            const t = searchTerms[Math.floor(Math.random() * searchTerms.length)]
+        if (msg.content.substr(config.prefix.length).startsWith('dilbert')) {
+            // Was a specific keyword used
+            let terms = ""
+            for (i in msg.content.substr(config.prefix.length+7).trim().split(" ")) {
+                terms += `${msg.content.substr(config.prefix.length+7).trim().split(" ")[i]}+`
+            }
+            // If no keywords were specified, select a random one
+            terms.length === 0 ? terms = searchTerms[Math.floor(Math.random() * searchTerms.length)] : null
             // If the search term returns base then select from the homepage, if not append the search query
-            const url = t === "base" ? baseurl :`${baseurl}search_results?terms=${t}`
+            const url = terms === "base" ? baseurl :`${baseurl}search_results?terms=${terms}`
             console.log(`${url}`.cyan)
             commands.dilbert(msg, url)
         } else if (msg.content.substr(1).startsWith('info')) {
